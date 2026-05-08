602e540ffe7e11114761551ec1e9bf1bf9c3b28d
Zixing Jiang <ZixingJiang@outlook.com>
2026-05-09 02:55:12 +0800

    chore: remove vendored vggt source

--- repo/vggt/vggt/dependency/__init__.py
+++
@@ -1,3 +0,0 @@
-from .track_modules.track_refine import refine_track
-from .track_modules.blocks import BasicEncoder, ShallowEncoder
-from .track_modules.base_track_predictor import BaseTrackerPredictor
